8453ae11fc53327629b5c1c6c0a4052fc65a44f2
evgenynefedov <e.nefyodoff@gmail.com>
2023-08-23 19:22:36 +0300

    search form - just elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+
+import Container from '@mui/material/Container';
 import Search from './components/Search/Search'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
-    <div className="App">
-      Fitness Beer App
+    <Container component="main" maxWidth="xs">
+      
       <Search />
 
-    </div>
+    </Container>
   );
 }
 
--- src/components/Search/Form/Form.js
+++ src/components/Search/Form/Form.js
@@ -1,10 +1,52 @@
+import React, { useEffect } from 'react';
+import Stack from '@mui/material/Stack';
 
-function Form() {
-  return (
-    <div className="form">
-      form will be here
-    </div>
-  );
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import BarsQuantity from './BarsQuantity';
+import Difficulty from './Difficulty';
+import Mode from './Mode';
+import Submit from './Submit';
+
+export default function Form() {
+
+    const [difficulty, setDifficulty] = React.useState(50);
+    const [barsQuantity, setBarsQuantity] = React.useState(2);
+    const [mode, setMode] = React.useState('foot');
+
+    const handleDistance = (event, newValue) => {
+        setDifficulty(newValue);
+    };
+
+    const handleBarsQuantity = (event, newValue) => {
+        if(newValue < 2){
+            setBarsQuantity(2);
+        } else {
+            setBarsQuantity(newValue);
+        }
+    };
+
+    const handleMode = (event, newValue) => {
+        if (newValue !== null) {
+            setMode(newValue);
+        }
+    };
+
+    useEffect(() => {
+        console.log(`difficulty: ${difficulty}, barsQuantity ${barsQuantity}, mode ${mode}`)
+    }, [difficulty, barsQuantity, mode])
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Typography variant="h5" align="center" sx={{mt:3, mb:5}} component="h1">Search Your Fitness Beer Route</Typography>
+            
+            <Stack spacing={2} direction="column" alignItems="center">
+                <Difficulty difficulty={difficulty} handleChange={handleDistance}/>
+                <BarsQuantity barsQuantity={barsQuantity} handleChange={handleBarsQuantity} />
+                <Mode mode={mode} handleChange={handleMode} />
+                <Submit />
+            </Stack>
+        </Container>
+    );
 }
-
-export default Form;
